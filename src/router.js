bc0b686e23485849808892b2bb4e9ed13f7662c8
Jaoued <68811444+Jaoued-mtd@users.noreply.github.com>
2020-11-14 18:04:44 +0100

    favicon and title

--- src/router.js
+++ src/router.js
@@ -13,18 +13,44 @@ import Register from "./pages/user/RegisterForm.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: Home },
-    { path: "/annonces", component: AdsPage },
-    { path: "/annonces/:id", component: AdsDetail },
-    { path: "/profil", component: Profil, meta: { requiresAuth: true } },
-    { path: "/create", component: AdsCreate, meta: { requiresAuth: true } },
+    { path: "/", component: Home, meta: { title: "La Bredouille | Accueil" } },
+    {
+      path: "/annonces",
+      component: AdsPage,
+      meta: { title: "La Bredouille | Annonces" },
+    },
+    {
+      path: "/annonces/:id",
+      component: AdsDetail,
+      meta: { title: "La Bredouille | Annonce" },
+    },
+    {
+      path: "/profil",
+      component: Profil,
+      meta: { title: "La Bredouille | Profil", requiresAuth: true },
+    },
+    {
+      path: "/create",
+      component: AdsCreate,
+      meta: { title: "La Bredouille | Création", requiresAuth: true },
+    },
     { path: "/:notFound(.*)", component: NotFound },
-    { path: "/login", component: Login, meta: { requiresUnauth: true } },
-    { path: "/register", component: Register, meta: { requiresUnauth: true } },
+    {
+      path: "/login",
+      component: Login,
+      meta: { requiresUnauth: true, title: "La Bredouille | Connexion" },
+    },
+    {
+      path: "/register",
+      component: Register,
+      meta: { requiresUnauth: true, title: "La Bredouille | Inscription" },
+    },
   ],
 });
 
 router.beforeEach(function(to, _, next) {
+  document.title = to.meta.title;
+
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next("/login");
   } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
