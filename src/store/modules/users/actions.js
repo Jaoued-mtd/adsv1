9c435371fbf4b58476b29a5a6580471774d25d49
Jaoued <68811444+Jaoued-mtd@users.noreply.github.com>
2020-11-14 17:57:26 +0100

    url api

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -1,32 +1,4 @@
 export default {
-  // creer une user
-  // async createUser(context, payload) {
-  //   const adData = {
-  //     username: payload.username,
-  //     phone: payload.phone,
-  //     userId: payload.userId,
-  //     email: payload.email,
-  //   };
-
-  //   const response = await fetch(
-  //     `https://peche-app.firebaseio.com/users.json`,
-  //     {
-  //       method: "POST",
-  //       body: JSON.stringify(adData),
-  //     }
-  //   );
-
-  //   const responseData = await response.json();
-
-  //   if (!response.ok) {
-  //     const error = new Error(
-  //       responseData.message || "Failed to send request."
-  //     );
-  //     throw error;
-  //   }
-
-  //   context.commit("createUser", adData);
-  // },
   async loadUsers(context) {
     var myHeaders = new Headers();
     myHeaders.append(
